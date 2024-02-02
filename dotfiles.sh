8a312bb0e80f29a3953a1461c988d7f7b5577649
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2024-02-01 20:50:18 -1000

    Organize dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,29 +5,25 @@ echo "Configuring dotfiles..."
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle --file ~/.dotfiles/Brewfile
 
-# Shell
-rm ~/.zprofile
-rm ~/.zshrc
-ln -s ~/.dotfiles/.zprofile ~/.zprofile
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+# zsh
+rm ~/.zprofile && ln -s ~/.dotfiles/.zprofile ~/.zprofile
+rm ~/.zshrc && ln -s ~/.dotfiles/.zshrc ~/.zshrc
 source ~/.zshrc
 
-# Git
-rm ~/.gitconfig
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+# git
+rm ~/.gitconfig && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-# Tmux
+# tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-rm ~/.tmux.conf
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+rm ~/.tmux.conf && ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
-# Neovim
+# nvim
 rm -rf ~/.config/nvim/
 rm -rf ~/.local/share/nvim/
 rm -rf ~/.local/state/nvim/
 git clone https://github.com/cmdecker95/nvim ~/.config/nvim
 
-# QMK
+# qmk
 qmk setup cmdecker95/qmk_firmware -y
 qmk config user.keyboard=planck/rev7
 qmk config user.keymap=cmdecker95
