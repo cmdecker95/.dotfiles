d1e69dc9182218c0be0767dc1f8fd8f17f04cac4
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2026-09-21 10:37:39 -0400

    add comments to dotfiles installer

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,19 +1,21 @@
 #!/bin/zsh
-echo "Configuring dotfiles..."
+echo "🔧🔧🔧"
 
+# install homebrew if not installed, then install brews from brefile
 if ! command -v brew >/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle --file "$HOME/.dotfiles/Brewfile"
 
+# plant configs on host
+mkdir -p "$HOME/.config"
+ln -sfn "$HOME/.dotfiles/starship.toml" "$HOME/.config/starship.toml"
 ln -sfn "$HOME/.dotfiles/.zprofile" "$HOME/.zprofile"
 ln -sfn "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
 ln -sfn "$HOME/.dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
-mkdir -p "$HOME/.config"
-ln -sfn "$HOME/.dotfiles/starship.toml" "$HOME/.config/starship.toml"
-
+# rice up tmux
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 fi
@@ -23,4 +25,4 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   git clone git@github.com:cmdecker95/nvim.git "$HOME/.config/nvim"
 fi
 
-echo "Successfully configured dotfiles!"
+echo "🎉🎉🎉"
